914ae7730c2cd96a5b6c8e652ccf9285389b8ce7
Jesse Harding <jesse.alexander.harding@gmail.com>
2019-01-27 22:48:05 -0500

    Update sketch.js
    onmousepress & transparent background

--- mmp210/sketch.js
+++ mmp210/sketch.js
@@ -1,23 +1,27 @@
+var r1 = 0;
+var r2 = 0;
+
 function setup() {
-  //createCanvas(720, 400);
-  noStroke();
-  rectMode(CENTER);
+    noStroke();
+    rectMode(CENTER);
     var cnv = createCanvas(200, 200);
-  //var cnv = createCanvas(windowWidth, windowHeight);
-  cnv.style('display', 'block');
-  cnv.parent('content');
-  cnv.position(100,100);
+    //var cnv = createCanvas(windowWidth, windowHeight);
+    cnv.style('display', 'block');
+    cnv.parent('content');
+    cnv.position(-width/2,0);
 }
 
 function draw() {
-  background(230);
-
-  var r1 = map(mouseX, 0, width, 0, height);
-  var r2 = height - r1;
+    background(119,119,119);
+    if (mouseY > 0 && mouseY < height && mouseX > 0 && mouseX < height && mouseIsPressed){
+    r1 = map(mouseX, 0, width, 0, height);
+    
+}
+r2 = height - r1;
   
-  fill(237,34,93,r1);
-  rect(width/2 + r1/2, height/2, r1, r1);
+    fill(237,34,93,r1);
+    rect(width/2 + r1/2, height/2, r1, r1);
   
-  fill(237,34,93,r2);
-  rect(width/2 - r2/2, height/2, r2, r2);
+    fill(237,34,93,r2);
+    rect(width/2 - r2/2, height/2, r2, r2);
 }
