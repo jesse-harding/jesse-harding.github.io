5e3d86cb4fda9258fd70d8507728fcd9c03c122d
Jesse Harding <jesse.alexander.harding@gmail.com>
2024-09-22 09:11:23 -0400

    Add files via upload

---
+++ p5demos/multi-sketch/sketch1.js
@@ -0,0 +1,9 @@
+function setup() {
+  let cnv1 = createCanvas(100, 100);
+  cnv1.parent("container1");
+  cnv1.position(0, 0);
+}
+
+function draw() {
+  background(220);
+}
---
+++ p5demos/multi-sketch/sketch2.js
@@ -0,0 +1,9 @@
+function setup() {
+  let cnv2 = createCanvas(100, 100);
+  cnv2.parent("container2");
+  cnv2.position(150, 100);
+}
+
+function draw() {
+  background(50);
+}
